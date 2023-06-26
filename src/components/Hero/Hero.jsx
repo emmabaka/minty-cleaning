b65f3b145a687fbc310b06cdbcc22dcfcc61c2c0
emmabaka <bakaemma88@gmail.com>
2023-06-26 22:22:19 +0300

    add hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import hero from "../../assets/hero.png";
+import heroDesktop from "../../assets/hero-desktop.png";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import css from "./Hero.module.css";
+
+const Hero = () => {
+  const isDesktop = useMediaQuery("only screen and (min-width : 1339px)");
+  return (
+    <section className={css.hero}>
+      <div className={`${css.heroWrap} container`}>
+        <h1 className={css.title}>
+          Відпустіть турботи,{" "}
+          <span className={css.accent}>довірте прибирання професіоналам!</span>
+        </h1>
+        <img
+          className={css.img}
+          src={isDesktop ? heroDesktop : hero}
+          alt="Hero image"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
