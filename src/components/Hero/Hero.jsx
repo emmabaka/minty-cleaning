de3dcdadb576e9a04f12c1677f5ead5e429a5ca1
emmabaka <bakaemma88@gmail.com>
2023-06-30 15:21:35 +0300

    fix hero section styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 import hero from "../../assets/hero.png";
-import heroDesktop from "../../assets/hero-desktop.png";
+import heroDesktop from "../../assets/hero1x.png";
+import heroMonitor from "../../assets/hero2x.png";
 import { useMediaQuery } from "@uidotdev/usehooks";
 import css from "./Hero.module.css";
 
 const Hero = () => {
-  const isDesktop = useMediaQuery("only screen and (min-width : 1339px)");
+  const isMobile = useMediaQuery("only screen and (max-width : 1339px)");
+  const isDesktop = useMediaQuery(
+    "only screen and (min-width : 1339px) and (max-width : 1649px)"
+  );
+  const isMonitor = useMediaQuery("only screen and (min-width : 1649px)");
   return (
     <section className={css.hero}>
       <div className={`${css.heroWrap} container`}>
@@ -12,11 +17,13 @@ const Hero = () => {
           Відпустіть турботи,{" "}
           <span className="accent">довірте прибирання професіоналам!</span>
         </h1>
-        <img
-          className={css.img}
-          src={isDesktop ? heroDesktop : hero}
-          alt="Hero image"
-        />
+        {isMobile && <img className={css.img} src={hero} alt="Hero image" />}
+        {isDesktop && (
+          <img className={css.img} src={heroDesktop} alt="Hero image" />
+        )}
+        {isMonitor && (
+          <img className={css.img} src={heroMonitor} alt="Hero image" />
+        )}
       </div>
     </section>
   );
